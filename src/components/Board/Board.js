43b12d0ca8d12b83e5ac44a8eeb340957241e948
peter <peter.arth5@gmail.com>
2023-04-20 11:52:21 +0100

    on click colors change to red and yellow on board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -8,27 +8,62 @@ const Board = () => {
 
   const [boardArr, setBoardArr] = useState([
     [null, null, null, null, null, null, null],
-    [null, "red", null, null, null, null, null],
-    [null, null, null, "yellow", null, null, null],
+    [null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null],
     [null, null, null, null, null, null, null],
   ]);
+  const [playerRed, setPlayerRed] = useState("R");
+  const [playerYellow, setPlayerYellow] = useState("Y");
+  const [currentPlayer, setCurrentPlayer] = useState(playerRed);
+  const [gameOver, setGameOver] = useState(false);
 
   // columns are horizontal position
-  // rows are vertical
+  // rows are vertical position
+
+  function setPiece(rowCoord, columnCoord) {
+    // with coords can change board state
+    if (gameOver) {
+      return;
+    } // if game over, cannot set piece
+
+    const boardCopy = [...boardArr]; // making copy to so can update it
+
+    if (currentPlayer === playerRed) {
+      boardCopy[rowCoord][columnCoord] = "red";
+      setCurrentPlayer(playerYellow);
+    } else if (currentPlayer === playerYellow) {
+      boardCopy[rowCoord][columnCoord] = "yellow";
+      setCurrentPlayer(playerRed);
+    }
+    // if red turn, change piece to red and make it yellow turn
+    // if yellow turn, change piece to yellow turn
+
+    setBoardArr(boardCopy); // updating board state
+  }
+
   const tiles = [];
 
   const rows = 6;
   const columns = 7;
 
   for (let row = 0; row < rows; row++) {
+    let rowArr = [];
     for (let column = 0; column < columns; column++) {
-      tiles.push(
-        <Tile color={boardArr[row][column]} key={row + " " + column} />
+      rowArr.push(
+        <Tile
+          color={boardArr[row][column]}
+          key={row + " " + column}
+          id={row + "-" + column}
+          setPiece={setPiece}
+        />
       );
     }
+    tiles.push(rowArr);
   }
+
+  console.log("TILES", tiles);
 
   return <div className="board">{tiles}</div>;
 };
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./Tile.css";
 
-const Tile = ({ color }) => {
+const Tile = ({ color, id, setPiece }) => {
+  // console.log(id);
+
+  let coords = id.split("-");
+  let rowCoord = Number(coords[0]);
+
+  let columnCoord = Number(coords[1]);
+
   return (
     <div
       className="tile"
       style={{
         backgroundColor: color,
       }}
+      onClick={() => setPiece(rowCoord, columnCoord)}
     ></div>
   );
 };
